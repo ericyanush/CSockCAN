5df9a036e27746627367ec6ccade85dd39499a81
Eric Yanush <ericyanush@gmail.com>
2016-03-06 19:27:43 -0800

    initial bridge needed for fma

--- csockcan/csockcan.c
+++ csockcan/csockcan.c
@@ -1,4 +1,13 @@
 //
 // Created by eric on 3/6/16.
 //
+#include "csockcan.h"
 
+int32_t getIfIndexForIfName(const char* name, int32_t socketHandle)
+{
+    struct ifreq ifr;
+    strcpy(ifr.ifr_name, name);
+
+    ioctl(socketHandle, SIOCGIFINDEX, &ifr);
+    return ifr.ifr_ifindex;
+}
--- csockcan/csockcan.h
+++ csockcan/csockcan.h
@@ -1,5 +1,7 @@
 #include <stdlib.h>
 #include <unistd.h>
+#include <stdint.h>
+#include <stdbool.h>
 #include <string.h>
 
 #include <net/if.h>
@@ -10,3 +12,16 @@
 #include <linux/can.h>
 #include <linux/can/raw.h>
 
+/**
+ @brief C Bridging methods for Swift to communicate using socketcan interfaces
+ */
+
+/**
+ @brief Retrieve the Interface Index for an interface name
+
+ @param name: The name of the interface to search for
+ @param socketHandle: The socket handle to use during the search.
+
+ @return The interface index of the interface if found, -1 otherwise (same as ioctl)
+ */
+int32_t getIfIndexForIfName(const char* name, int32_t socketHandle);
